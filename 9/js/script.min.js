51c845d4ef84a8683a55741697bf71da0de69871
Keks <keks@htmlacademy.ru>
2022-09-06 09:09:55 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/script.min.js
@@ -0,0 +1,1 @@
+const navToggle=document.querySelector(".navigation__toggle"),nav=document.querySelector(".navigation");nav.classList.remove("navigation--nojs"),navToggle.addEventListener("click",(()=>{nav.classList.toggle("navigation--closed"),nav.classList.toggle("navigation--opened")}));
